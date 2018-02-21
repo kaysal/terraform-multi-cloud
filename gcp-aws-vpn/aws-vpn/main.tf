8a8f9e155eb48d7daae8df6b9d190554bbc754e4
Kayode Salawu <salawu@gmail.com>
2018-02-21 20:48:02 +0000

    changes

--- gcp-aws-vpn/aws-vpn/main.tf
+++ gcp-aws-vpn/aws-vpn/main.tf
@@ -11,6 +11,9 @@ resource "aws_vpc" "vpc" {
     cidr_block = "${var.cidr_block}"
     enable_dns_support = "${var.enable_dns_support}"
     enable_dns_hostnames = "${var.enable_dns_hostnames}"
+    tags {
+        Name = "${var.name}-demo"
+    }
 }
 
 # Create a private subnet to laucnh our DB into
